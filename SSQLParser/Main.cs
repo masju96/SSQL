960755cbd507cab9fa6baff5f910cca851189d61
Juan Mas <jumas@ucm.es>
2017-11-22 19:59:21 +0100

    First commit

---
+++ SSQLParser/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//compile with: /unsafe 
+
+namespace SSQLParser
+{
+    public class Spm
+    {
+        unsafe static void Main(string[] args)
+		{
+            int zero = 0;
+            int* i = &zero;
+            incr(&i);
+            System.Console.Write(i);
+		}
+
+        unsafe static private void incr(int* i)
+        {
+            i++;
+        }
+    }
+}
